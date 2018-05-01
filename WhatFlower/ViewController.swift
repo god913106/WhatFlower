33df6789d51501aa0b6618af5d868175ec213666
楊崇平 <god913106@gmail.com>
2018-05-01 18:17:14 +0800

    299. [Challenge] - Set up a UIImagePickerController

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    //建立一個imagePickController
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //設定delegate
+        imagePicker.delegate = self
+    }
+    
+    //取得圖片
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        /*
+         設定圖片來源：
+         (1).camera 相機
+         (2).photoLibrary 相冊
+         (3).savedPhotosAlbum 保存的圖片，範圍比相簿大
+         */
+        imagePicker.sourceType = .savedPhotosAlbum
+        //設定圖片是否編輯
+        imagePicker.allowsEditing = false
+        //顯示圖片
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
